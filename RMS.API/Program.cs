930bbd1ea082606571e8e7d93830abbd35cc1bab
naveddd3 <mohammadnaved1517@gmail.com>
2025-09-28 12:19:08 +0530

    d

--- RMS.API/Program.cs
+++ RMS.API/Program.cs
@@ -21,7 +21,7 @@ internal class Program
         // Add services to the container.
 
         builder.Services.AddDbContext<AppDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionConnection")));
 
 
         #region Repositories
@@ -90,8 +90,14 @@ internal class Program
             c.AddSecurityRequirement(securityRequirement);
         });
 
-
         builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
+        //var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+        //builder.WebHost.ConfigureKestrel(options =>
+        //{
+        //    options.ListenAnyIP(int.Parse(port));
+        //});
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
@@ -100,12 +106,13 @@ internal class Program
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
+        //if (app.Environment.IsDevelopment())
+        //{
+        //    app.UseSwagger();
+        //    app.UseSwaggerUI();
+        //}
+        app.UseSwagger();
+        app.UseSwaggerUI();
         app.UseHttpsRedirection();
         app.UseAuthentication();
         app.UseAuthorization();
